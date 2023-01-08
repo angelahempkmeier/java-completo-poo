5316ed3b7d6b1abb1fb3a32ea8fbeeeb4e8e7648
aggsdp <aggsdpz@gmail.com>
2023-01-08 10:32:44 -0300

    redondezas de uma matriz

--- redondezasMatriz/src/Main.java
+++ redondezasMatriz/src/Main.java
@@ -7,7 +7,29 @@ public class Main {
         int m = Integer.parseInt(sc.nextLine());
         int n = Integer.parseInt(sc.nextLine());
 
-        
+        int[][] mat = new int[m][n];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                mat[i][j] = Integer.parseInt(sc.nextLine());
+            }
+            System.out.println();
+        }
+
+        int number = Integer.parseInt(sc.nextLine());
+        int esquerda = 0, direita = 0, cima = 0, baixo = 0;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(mat[i][j] == number){
+                    esquerda = mat[i][j-1];
+                    direita = mat[i][j+1];
+                    cima = mat[i-1][j];
+                    baixo = mat[i+1][j];
+                }
+            }
+        }
+        System.out.println("Number: " + number);
+        System.out.println("Cima: " + cima + " Baixo: " + baixo + " Esquerda: " + esquerda + " Direita: " + direita);
 
     }
 }
