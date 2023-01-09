6f80d603d138c6f8715c773995a2301bd8d047a8
aggsdp <aggsdpz@gmail.com>
2023-01-09 19:44:45 -0300

    datas

---
+++ operacoesComDatas/src/Main.java
@@ -0,0 +1,34 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instanciação
+        LocalDate d01 = LocalDate.now(); //data sem tempo
+        LocalDateTime d02 = LocalDateTime.now(); //data hora local
+        Instant d03 = Instant.now(); //data hora global, em london
+
+        //texto iso para hora
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
+
+        LocalDate d08 = LocalDate.parse("20/10/2020", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04);
+        System.out.println(d05);
+        System.out.println(d06);
+        System.out.println(d07);
+        System.out.println(d08);
+
+
+
+
+    }
+}
