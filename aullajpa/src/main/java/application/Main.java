35f3044cca204aed523fd517d253901358819189
aggsdp <aggsdpz@gmail.com>
2023-03-15 19:11:30 -0300

    maven

---
+++ aullajpa/src/main/java/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import dominio.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "Carlos", "carlos@gmail");
+        Pessoa p2 = new Pessoa(2, "Alberto", "alberto@gmail");
+        Pessoa p3 = new Pessoa(3, "Camila", "camila@gmail");
+
+        System.out.println(p1);
+
+    }
+}
---
+++ jpa-aula1/src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import dominio.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "Carlos", "carlos@gmail");
+        Pessoa p2 = new Pessoa(2, "Alberto", "alberto@gmail");
+        Pessoa p3 = new Pessoa(3, "Camila", "camila@gmail");
+
+
+    }
+}
