140b01eb8f190374a5e752a4a870255dff74843a
aggsdp <aggsdpz@gmail.com>
2023-03-15 19:20:58 -0300

    persistindo uma pessoa no banco

--- aullajpa/src/main/java/application/Main.java
+++ aullajpa/src/main/java/application/Main.java
@@ -2,13 +2,26 @@ package application;
 
 import dominio.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlos", "carlos@gmail");
-        Pessoa p2 = new Pessoa(2, "Alberto", "alberto@gmail");
-        Pessoa p3 = new Pessoa(3, "Camila", "camila@gmail");
+        Pessoa p1 = new Pessoa(null, "Carlos", "carlos@gmail");
+        Pessoa p2 = new Pessoa(null, "Alberto", "alberto@gmail");
+        Pessoa p3 = new Pessoa(null, "Camila", "camila@gmail");
 
-        System.out.println(p1);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Finished.");
+
+
 
     }
 }
