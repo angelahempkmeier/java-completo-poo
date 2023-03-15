01dfaae2c3e463b7efd20b19f5d7dcaff0f9c8ae
aggsdp <aggsdpz@gmail.com>
2023-03-15 19:26:02 -0300

    recuperando pessoa

--- aullajpa/src/main/java/application/Main.java
+++ aullajpa/src/main/java/application/Main.java
@@ -8,18 +8,17 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Carlos", "carlos@gmail");
-        Pessoa p2 = new Pessoa(null, "Alberto", "alberto@gmail");
-        Pessoa p3 = new Pessoa(null, "Camila", "camila@gmail");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
+
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
         em.getTransaction().commit();
         System.out.println("Finished.");
+        em.close();
+        emf.close();
 
 
 
